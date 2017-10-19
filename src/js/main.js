320299d64a91a03ae53dc0a35b85f1e2f123d7da
Unknown <thomas.guillet35@hotmail.fr>
2017-10-19 12:58:28 +0200

    fix key pressed WIP

--- src/js/main.js
+++ src/js/main.js
@@ -37,7 +37,6 @@ function playAudioTransition(){
 }
 
 intro.onclick = function(){
-	console.log('lol');
 	if(introStatus == 1){
 		intro.pause();
 		introStatus = 0;
@@ -57,7 +56,6 @@ start.onclick = function(){
 
 function play_note(note)
 {
-	console.log(accord);
 	if(accord == true){
 		if(note == 'c'){
 			const audio1 = document.getElementById("audio-c");
@@ -76,68 +74,168 @@ function play_note(note)
 	}else{
 		const audio = document.getElementById("audio-"+note);
 		audio.pause();
+		if(typeof noteVolume !== 'undefined'){
+    		clearInterval(noteVolume);
+		}
 	 	audio.currentTime = 0;
+	 	audio.volume = 1;
 		audio.play();
 		const pianoNote = document.getElementById(note);
 		pianoNote.className += " selected";
-		setTimeout(function() { pianoNote.classList.remove("selected"); }, 500);
 	}
+}
+
+function stop_note(note)
+{
+	const audio = document.getElementById("audio-"+note);
+	audio.volume = 1
+	var volume = audio.volume;
+    clearInterval(noteVolume);
+	var noteVolume = setInterval(stopNoteVolume, 0.5);
+    function stopNoteVolume() {
+    	if(statutNote[note] == false){
+	        if (volume > 0.01) {
+	            volume -= 0.01; 
+				audio.volume = volume;
+	        }else{
+				audio.volume = 0;
+				audio.pause();
+	            clearInterval(noteVolume);
+	        }
+    	}
+    }
+	const pianoNote = document.getElementById(note);
+	pianoNote.classList.remove("selected");
 }
 
 document.onkeypress = function (e) {
     e = e || window.event;
     if(e.keyCode == 119){
     	const notePressed = 'c';
-    	play_note(notePressed);
-		statutNote[notePressed] = true;
+    	if(statutNote[notePressed] == false){
+    		play_note(notePressed);
+			statutNote[notePressed] = true;
+    	}
     }else if(e.keyCode == 115){
     	const notePressed = 'c-';
-    	play_note(notePressed);
-		statutNote[notePressed] = true;
+    	if(statutNote[notePressed] == false){
+    		play_note(notePressed);
+			statutNote[notePressed] = true;
+    	}
     }else if(e.keyCode == 120){
     	const notePressed = 'd';
-    	play_note(notePressed);
-		statutNote[notePressed] = true;
+    	if(statutNote[notePressed] == false){
+    		play_note(notePressed);
+			statutNote[notePressed] = true;
+    	}
     }else if(e.keyCode == 100){
     	const notePressed = 'd-';
-    	play_note(notePressed);
-		statutNote[notePressed] = true;
+    	if(statutNote[notePressed] == false){
+    		play_note(notePressed);
+			statutNote[notePressed] = true;
+    	}
     }else if(e.keyCode == 99){
     	const notePressed = 'e';
-    	play_note(notePressed);
-		statutNote[notePressed] = true;
+    	if(statutNote[notePressed] == false){
+    		play_note(notePressed);
+			statutNote[notePressed] = true;
+    	}
     }else if(e.keyCode == 118){
     	const notePressed = 'f';
-    	play_note(notePressed);
-		statutNote[notePressed] = true;
+    	if(statutNote[notePressed] == false){
+    		play_note(notePressed);
+			statutNote[notePressed] = true;
+    	}
     }else if(e.keyCode == 103){
     	const notePressed = 'f-';
-    	play_note(notePressed);
-		statutNote[notePressed] = true;
+    	if(statutNote[notePressed] == false){
+    		play_note(notePressed);
+			statutNote[notePressed] = true;
+    	}
     }else if(e.keyCode == 98){
     	const notePressed = 'g';
-    	play_note(notePressed);
-		statutNote[notePressed] = true;
+    	if(statutNote[notePressed] == false){
+    		play_note(notePressed);
+			statutNote[notePressed] = true;
+    	}
     }else if(e.keyCode == 104){
     	const notePressed = 'g-';
-    	play_note(notePressed);
-		statutNote[notePressed] = true;
+    	if(statutNote[notePressed] == false){
+    		play_note(notePressed);
+			statutNote[notePressed] = true;
+    	}
     }else if(e.keyCode == 110){
     	const notePressed = 'a';
-    	play_note(notePressed);
-		statutNote[notePressed] = true;
+    	if(statutNote[notePressed] == false){
+    		play_note(notePressed);
+			statutNote[notePressed] = true;
+    	}
     }else if(e.keyCode == 106){
     	const notePressed = 'a-';
-    	play_note(notePressed);
-		statutNote[notePressed] = true;
+    	if(statutNote[notePressed] == false){
+    		play_note(notePressed);
+			statutNote[notePressed] = true;
+    	}
     }else if(e.keyCode == 44){
     	const notePressed = 'h';
-    	play_note(notePressed);
-		statutNote[notePressed] = true;
+    	if(statutNote[notePressed] == false){
+    		play_note(notePressed);
+			statutNote[notePressed] = true;
+    	}
     }
 	
 };
 
 document.onkeyup = function (e) {
-console.log(e.keyCode);
+	console.log('up');
+	e = e || window.event;
+    if(e.keyCode == 87){
+    	const notePressed = 'c';
+    	stop_note(notePressed);
+		statutNote[notePressed] = false;
+    }else if(e.keyCode == 83){
+    	const notePressed = 'c-';
+    	stop_note(notePressed);
+    	statutNote[notePressed] = false;
+    }else if(e.keyCode == 88){
+    	const notePressed = 'd';
+    	stop_note(notePressed);
+    	statutNote[notePressed] = false;
+    }else if(e.keyCode == 68){
+    	const notePressed = 'd-';
+    	stop_note(notePressed);
+    	statutNote[notePressed] = false;
+    }else if(e.keyCode == 67){
+    	const notePressed = 'e';
+    	stop_note(notePressed);
+    	statutNote[notePressed] = false;
+    }else if(e.keyCode == 86){
+    	const notePressed = 'f';
+    	stop_note(notePressed);
+    	statutNote[notePressed] = false;
+    }else if(e.keyCode == 71){
+    	const notePressed = 'f-';
+    	stop_note(notePressed);
+    	statutNote[notePressed] = false;
+    }else if(e.keyCode == 66){
+    	const notePressed = 'g';
+    	stop_note(notePressed);
+    	statutNote[notePressed] = false;
+    }else if(e.keyCode == 72){
+    	const notePressed = 'g-';
+    	stop_note(notePressed);
+    	statutNote[notePressed] = false;
+    }else if(e.keyCode == 78){
+    	const notePressed = 'a';
+    	stop_note(notePressed);
+    	statutNote[notePressed] = false;
+    }else if(e.keyCode == 74){
+    	const notePressed = 'a-';
+    	stop_note(notePressed);
+    	statutNote[notePressed] = false;
+    }else if(e.keyCode == 188){
+    	const notePressed = 'h';
+    	stop_note(notePressed);
+    	statutNote[notePressed] = false;
+    }
 };
